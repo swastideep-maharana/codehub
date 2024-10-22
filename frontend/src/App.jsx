c84ed5041e306190631481b123d44d3701f1a8c2
swastideep maharana <swastideep67maharana@gmail.com>
2024-10-22 06:31:15 +0530

    #setting up the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import ExplorePage from "./pages/ExplorePage";
+import HomePage from "./pages/HomePage";
+import LikesPage from "./pages/LikesPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <h1 className="text-3xl text-red-500">hello from the home page</h1>;
+  return (
+    <div className="flex text-white ">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto">
+
+    </aside>
+  )
+};
+
+export default Sidebar;
